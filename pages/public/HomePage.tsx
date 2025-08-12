60569289a0ef1759a251ead614b89e60da685ce9
João Victor Rodrigues Pereira Constantino <39504253+JVConstantino@users.noreply.github.com>
2025-08-12 09:10:53 -0300

    Update HomePage.tsx

--- pages/public/HomePage.tsx
+++ pages/public/HomePage.tsx
@@ -77,7 +77,7 @@ const HeroSection = () => {
       <div className="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 pt-20 pb-24">
         <div className="text-center">
             <AnimateOnScroll>
-                <img src={LOGO_URL} alt="Rezuski Imóveis Logo" className="h-32 mx-auto mb-6 object-contain" />
+                <img src={LOGO_URL} alt="Rezuski Imóveis Logo" className="h-60 mx-auto mb-6 object-contain" />
             </AnimateOnScroll>
             <AnimateOnScroll delay={50}>
                 <h1 className="text-4xl md:text-6xl font-extrabold text-gray-50 leading-tight">
